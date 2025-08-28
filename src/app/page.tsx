22471a854fb8fe2f345301d460ff71bf150fb929
Josue D. Navarro L. <josuednavarrol6@gmail.com>
2025-08-28 23:05:32 +0000

    en el txt del usuario tambien quitale el mensaje que tre por defecto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -104,7 +104,6 @@ export default function LoginPage() {
             <Input 
               id="username" 
               type="text" 
-              placeholder={t('username_placeholder')}
               required 
               value={username}
               onChange={(e) => setUsername(e.target.value)}
